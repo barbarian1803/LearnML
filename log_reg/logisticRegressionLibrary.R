cc52507a5b7e256faf5c2a7d92a521158b347562
barbarian1803 <bharata180@gmail.com>
2016-05-02 14:56:08 +0900

    Changes to be committed: 	modified:   log_reg/logisticRegressionLibrary.R

--- log_reg/logisticRegressionLibrary.R
+++ log_reg/logisticRegressionLibrary.R
@@ -3,3 +3,12 @@ dataset$V0 <- rep(1,length(dataset))
 training <- as.matrix(dataset[1:300,c(5,2,3,4,1)])
 test <- as.matrix(dataset[301:400,c(5,2,3,4,1)])
 
+h <- function(theta,X){
+  X%*%theta
+}
+
+
+GradientDescent <- function(){
+  
+}
+
