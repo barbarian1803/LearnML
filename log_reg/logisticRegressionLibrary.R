0cc532d03ae08d85c89fe92a5c7ba3d69e7b14d2
barbarian1803 <bharata180@gmail.com>
2016-05-02 14:24:15 +0900

    Logistic regression Changes to be committed: 	new file:   log_reg/dataset.csv 	new file:   log_reg/logisticRegressionLibrary.R

---
+++ log_reg/logisticRegressionLibrary.R
@@ -0,0 +1,5 @@
+dataset <- read.table("log_reg/dataset.csv",header=TRUE,sep=",")
+dataset$V0 <- rep(1,length(dataset))
+training <- as.matrix(dataset[1:300,c(5,2,3,4,1)])
+test <- as.matrix(dataset[301:400,c(5,2,3,4,1)])
+
